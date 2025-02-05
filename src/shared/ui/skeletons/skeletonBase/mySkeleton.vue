a3775de61e1e3cf9b961d19acb6909306e26279b
Cheasezz <yuradolgo@gmail.com>
2025-02-05 15:07:42 +0300

    add randomize for skeleton

--- src/shared/ui/skeletons/skeletonBase/mySkeleton.vue
+++ src/shared/ui/skeletons/skeletonBase/mySkeleton.vue
@@ -1,7 +1,19 @@
 <script lang="ts" setup>
 import './style.scss'
+
+const props = defineProps({
+  randomWidth: {
+    type: Boolean,
+    default: false,
+  },
+})
+// Случайная ширина в диапозоне от 20% до 100%
+const style = props.randomWidth?`width: ${Math.random() * (100 - 20) + 20}%;`:""
 </script>
 
 <template>
-  <div class="skeleton"></div>
+  <div
+    class="skeleton"
+    :style="style"
+  ></div>
 </template>
--- src/shared/ui/skeletons/skeletonText/skeletonText.vue
+++ src/shared/ui/skeletons/skeletonText/skeletonText.vue
@@ -9,10 +9,11 @@ defineProps<{
 
 <template>
   <div class="skeleton-text">
-  <mySkeleton
-  class="skeleton-text__item"
-    v-for="n in strCount"
-    :key="n"
-  />
-</div>
+    <mySkeleton
+      class="skeleton-text__item"
+      v-for="n in strCount"
+      :key="n"
+      :random-width="true"
+    />
+  </div>
 </template>
